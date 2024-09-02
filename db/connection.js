b61fe461a33815664787b805fb8d8c978a1d96f2
SRawatDev <sumitrawat1102000@gmail.com>
2024-09-02 20:54:58 +0530

    chnage into to string

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connection=async()=>{
     try {
-        await mongoose.connect(process.env.db)
+        await mongoose.connect(process.env.db.toString())
         console.log('mongodb is connected',mongoose.connection.host)
     } catch (error) {
         console.log(error)
