410ccc3955321b3b036fa41437595c52410763ab
SRawatDev <sumitrawat1102000@gmail.com>
2024-09-02 20:57:54 +0530

    fix cores error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8000;
 // const app = express();
 dotenv.config()
 app.use(cors({
-    origin: "https://chat-nest-zeta.vercel.app/",
+    origin: "*",
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization"
